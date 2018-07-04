6e6ab1f0b9e5b5a666479e4c5eb8184b964e65bb
Randy's Mac <randydavidl78@gmail.com>
2018-07-04 12:53:40 -0400

    Refactor to group all modal variables together

--- js/app.js
+++ js/app.js
@@ -2,54 +2,55 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 		"use strict";
+
 	// ************************************************************
 	//				Modal HTML
 	// ************************************************************	
-	const modal2 = document.createElement('div');
-	modal2.classList.add("overlay");
-	modal2.innerHTML = `
-	<div class="modal">
-		<button class="btn--close">
-			<span>Close</span>
-			<svg class="icn">
-				<use xlink:href="#close"/>
-			</svg>
-		</button>
-		<button class="btn--next">
-			<span>Next</span>
-			<svg class="icn">
-				<use xlink:href="#next"/>
-			</svg>
-		</button>
-		<button class="btn--previous">
-			<span>Previous</span>
-			<svg class="icn">
-				<use xlink:href="#previous"/>
-			</svg>
-		</button>
-		<div class="modal__inner">
-			<h2>Attention!</h2>
-			<p>This is a test modal</p>
-			<button class="btn--modal">Close</button>
-		</div>
-	</div>	
-	`
-	const body = document.getElementsByTagName('body')[0];
-	body.appendChild(modal2);
+	const overlay = document.createElement('div');
+	overlay.classList.add("overlay");
+	overlay.innerHTML = `
+		<div class="modal">
+			<button class="btn--close">
+				<span>Close</span>
+				<svg class="icn">
+					<use xlink:href="#close"/>
+				</svg>
+			</button>
+			<button class="btn--next">
+				<span>Next</span>
+				<svg class="icn">
+					<use xlink:href="#next"/>
+				</svg>
+			</button>
+			<button class="btn--previous">
+				<span>Previous</span>
+				<svg class="icn">
+					<use xlink:href="#previous"/>
+				</svg>
+			</button>
+			<div class="modal__inner">
+				<h2>Attention!</h2>
+				<p>This is a test modal</p>
+				<button class="btn--modal">Close</button>
+			</div>
+		</div>	
+	`;
+	// add modal to the body off index.html
+	document.getElementsByTagName('body')[0].appendChild(overlay);
+	// variables for modal components
+	const modal = document.querySelector(".modal");
+	const modalContent = modal.querySelector(".modal__inner");
+	const closeModal = modal.querySelector(".btn--close");
+	const nextModal = modal.querySelector(".btn--next");
+	const previousModal = modal.querySelector(".btn--previous");
+
 
 	// ************************************************************
 	//				Globals
 	// ************************************************************		
 	const employeeCount = 12;	// adjust this to grab more or less employees from API	
 	const wrapper = document.querySelector(".wrapper");
-	const overlay = document.querySelector(".overlay");
-	const modal = document.querySelector(".modal");
-	const modalContent = modal.querySelector(".modal__inner");
-	const closeModal = modal.querySelector(".btn--close");
-	const nextModal = modal.querySelector(".btn--next");
-	const previousModal = modal.querySelector(".btn--previous");
 	const sortUL = document.querySelector(".sort");
-	
 	let employees = [];
 	let currentEmployee = 0; // use to display correct employee in modal
 	let sortType = "first";  // use to tell what method to sort employees by
